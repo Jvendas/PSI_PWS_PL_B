8f076e40b47ba63ce591fcbc45cdf6dc25369a7f
Ana Cacao <2201159@my.ipleiria.pt>
2021-05-27 19:36:08 +0100

    atualizadas as rotas

--- weblogicmvc/webapp/router.php
+++ weblogicmvc/webapp/router.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: smendes
  * Date: 02-05-2016
  * Time: 11:18
  */
+
 use ArmoredCore\Facades\Router;
 
 /****************************************************************************
@@ -12,16 +14,34 @@ use ArmoredCore\Facades\Router;
  *  Use convention: controllerName/methodActionName
  ****************************************************************************/
 
-Router::get('/',			'HomeController/index');
-Router::get('home/',		'HomeController/index');
-Router::get('home/index',	'HomeController/index');
-Router::get('home/start',	'HomeController/start');
+Router::get('/',                                'HomeController/index');
+Router::get('home/',                            'HomeController/index');
+Router::get('home/index',                       'HomeController/index');
+Router::get('home/start',                       'HomeController/start');
+Router::get('home/admin',                       'AdminController/index');
 
+// admin
+Router::get('admin/',                           'AdminController/contas');
+
+// admin contas
+Router::get('admin/contas',                     'AdminController/contas');
+Router::post('conta/eliminar',                 'AccountsController/eliminar');
+Router::post('conta/editar',                   'AccountsController/editar');
+
+// admin aeroports
+Router::get('admin/aeroportos',                'AdminController/aeroportos');
+Router::post('aeroporto/criar',                'AirportController/criar');
+Router::post('aeroporto/eliminar',             'AirportController/eliminar');
+Router::post('aeroporto/editar',               'AirportController/editar');
+Router::post('aeroporto/atualizar',            'AirportController/atualizar');
+
+
+
+
+
+
+// tests
 Router::get('test/index',  'TestController/index');
-
-
-
-
 
 
 
